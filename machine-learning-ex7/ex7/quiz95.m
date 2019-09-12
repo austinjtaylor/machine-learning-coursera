7214cb494b68f9341f8b5f26c64feb042d04c31b
Austin Taylor <ajtaylor127@gmail.com>
2019-09-12 13:45:25 -0400

    Added assignments

---
+++ machine-learning-ex7/ex7/quiz95.m
@@ -0,0 +1,10 @@
+R = [0 1 0 1 0; 1 0 1 0 1; 0 1 0 1 0; 1 0 1 0 1; 0 1 0 1 0];
+C = A * B;
+total = 0;
+for i = 1:5
+    for j = 1:5
+        if (R(i,j) == 1)
+            total = total + C(i,j)
+        end
+    end
+end
